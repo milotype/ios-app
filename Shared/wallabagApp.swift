88539ff052f1e11cb77dddee460666d52b67e7a1
Marinel maxime <maxime.m62@gmail.com>
2020-08-07 22:39:18 +0200

    Prepare multiplatform

---
+++ Shared/wallabagApp.swift
@@ -0,0 +1,17 @@
+//
+//  wallabagApp.swift
+//  Shared
+//
+//  Created by maxime marinel on 07/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct wallabagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
